ccb2e23633fc6678074d26ea0289cf6202b46b57
renanNun <renannunes400@gmail.com>
2021-09-11 16:04:06 -0300

    Atualizando versão do node

--- app/tests/index.test.js
+++ app/tests/index.test.js
@@ -10,20 +10,20 @@ test('Pegar todos os usuários', async () => {
 
 test('Cadastrar um usuário', async () => {
   const response = await supertest(index)
-  .post('/users')
-  .send({
-    "name": "Super Usuário", 
-    "email": "superusuario@gmail.com",
-    "password": "cavalodeTeta123",
-    "confirmedPassword": "cavalodeTeta123"
-  });
+    .post('/users')
+    .send({
+      name: 'Super Usuário',
+      email: 'superusuario@gmail.com',
+      password: 'cavalodeTeta123',
+      confirmedPassword: 'cavalodeTeta123'
+    })
 
   expect(response.statusCode).toEqual(201)
 })
 
 test('Deletar um Usuário', async () => {
   const response = await supertest(index)
-  .delete('/users/49')
+    .delete('/users/49')
 
   expect(response.statusCode).toEqual(204)
 })
