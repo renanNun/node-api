bf462e8889039cd4f893c8ba09aef5664b461509
renanNun <renannunes400@gmail.com>
2021-09-11 16:02:10 -0300

    Adicionando mais testes unitários

--- app/tests/index.test.js
+++ app/tests/index.test.js
@@ -7,3 +7,23 @@ test('Pegar todos os usuários', async () => {
 
   expect(response.statusCode).toEqual(200)
 })
+
+test('Cadastrar um usuário', async () => {
+  const response = await supertest(index)
+  .post('/users')
+  .send({
+    "name": "Super Usuário", 
+    "email": "superusuario@gmail.com",
+    "password": "cavalodeTeta123",
+    "confirmedPassword": "cavalodeTeta123"
+  });
+
+  expect(response.statusCode).toEqual(201)
+})
+
+test('Deletar um Usuário', async () => {
+  const response = await supertest(index)
+  .delete('/users/49')
+
+  expect(response.statusCode).toEqual(204)
+})
